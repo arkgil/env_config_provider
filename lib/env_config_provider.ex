fcf33589bdccfe420bc34bbe9772519ebb635169
Arkadiusz Gil <arkadiusz@arkgil.net>
2018-10-11 00:06:40 +0200

    Document initial target behaviour

--- lib/env_config_provider.ex
+++ lib/env_config_provider.ex
@@ -2,22 +2,83 @@ defmodule EnvConfigProvider do
   @moduledoc """
   [Distillery](https://github.com/bitwalker/distillery) config provider reading configuration data
   from environment variables.
-  """
 
-  @behaviour Mix.Releases.Config.Provider
+  The information how system environment variables map to application environment variables is
+  contained in the schema. Schema is a map, where keys are strings with names of system environment
+  variables, and values are "access paths" to application environment variables. Example schema
+  looks like this:
 
-  @typedoc """
-  The name of system environment variable, a string.
-  """
-  @type env_var_name :: String.t()
+      %{
+        "PORT" => [:my_app, :http, :port],
+        "IP" => [:my_app, :http, :ip],
+        "API_KEY" => [:lib, :api_key]
+      }
 
-  @typedoc !"""
-           The value of system environment variable, a string or `nil` if variable is not set.
-           """
-  @type env_var_value :: String.t() | nil
+  When the config provider executes, it fetches the values of system environment variables, and
+  (if the variables are actually set) puts them in application environment according to given
+  access paths. If all of the variables from the schema above were set, executing the provider
+  would generate application environment equivalent to following:
 
-  @typedoc """
-  List of atoms describing the access path to application environment variable.
+      config :my_app, :http,
+        port: <value>,
+        ip: <value>
+
+      config :lib,
+        api_key: <value>
+
+  If the variables were not set, the provider would raise an error. Note that variable values are
+  always strings and are never converted to any other type.
+
+  The provider not only places values in application environment, but it deeply merges them with
+  existing values. Imagine the application environment like this before running the provider:
+
+      config :my_app, :http,
+        port: 12221,
+        ip: "127.0.0.1",
+        ssl: false
+
+      config :my_app, MyApp.Repo,
+        database: "db",
+        username: "my_app"
+
+  After running the provider with the schema from previous example, the resulting configuration
+  would look like this
+
+      config :my_app, :http,
+        port: <value>,
+        ip: <value>,
+        ssl: false
+
+      config :my_app, MyApp.Repo,
+        database: "db",
+        username: "my_app"
+
+      config :lib,
+        api_key: <value>
+
+  Deep merging is crucial, because other providers might run before this one, and simply setting the
+  values (especially under nested keys) could override variables set by these providers.
+
+  ## Installation & usage
+
+  Add this library and Distillery to your dependencies:
+
+      defp deps() do
+        [
+          {:distillery, "~> 2.0"},
+          {:env_config_provider, "~> 0.1"}
+        ]
+      end
+
+  After that, simply set this module as one of the config providers in your release configuration
+  and provide a schema as the only argument:
+
+      set config_providers: [
+        {EnvConfigProvider, [MyApp.EnvConfig.schema()]},
+        ...
+      ]
+
+  ## Access paths
 
   Application environment API allows to set variables scoped to the application name and one,
   top-level key. However, in Elixir a lot of libraries use nested keyword lists as values in
@@ -43,9 +104,27 @@ defmodule EnvConfigProvider do
   lists. In the example above, `:database` key points to a string and not a keyword list, so it's
   the last key in the path.
 
-  Note that the structure of the access path implies that it needs to contain at least two segments -
+  Note that the structure of the access path implies that it needs to contain at least two elements -
   the first one for the application name and the second one for the top-level key. Unfortunately,
-  this cannot be reflected in the type specification.
+  this cannot be reflected in the type specification for `t:app_env_access_path/0` type.
+  """
+
+  @behaviour Mix.Releases.Config.Provider
+
+  @typedoc """
+  The name of system environment variable, a string.
+  """
+  @type env_var_name :: String.t()
+
+  @typedoc !"""
+           The value of system environment variable, a string or `nil` if variable is not set.
+           """
+  @type env_var_value :: String.t() | nil
+
+  @typedoc """
+  List of atoms describing the access path to application environment variable.
+
+  Learn more from "Access paths" section in the documentation for this module.
   """
   @type app_env_access_path :: [atom(), ...]
 
